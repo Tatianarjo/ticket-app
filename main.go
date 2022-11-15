d37aca46984d3c98e9fb038b02d9b2da5c5356b2
Tatiana Johnson <Tatianarjo@gmail.com>
2022-11-14 21:49:37 -0600

    Understanding Range, Slices, string.fields

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	conferenceName := "Go Conference"
@@ -39,12 +42,22 @@ func main() {
 		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
-		fmt.Printf("These are all our bookings: %v\n", bookings)
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		//This loop ends when iterated over all elements of the bookings list
+		fmt.Printf("These first names of bookings are: %v\n", firstNames)
 	}
 
 	//var bookings = [50]string{}
 	//variables & types
+	// using an underscore _ is a blank Identifier it ignores a variable you don't want to use
 	//bookings[1] = "Anna Lee"
+	//RANGE - iterates over elements for different data structures(so not only arrays and slices)
+	// For arrays & slices, RANGE provides the index and value for each element
+	//string.Fields - Splits the string with white space as a separator and returns a slice with the split elements
 
 }
 
@@ -54,7 +67,7 @@ func main() {
 //Slices are more flexible - variable-length or get a sub-array of its own
 //Slices are also index-based and have a size, but is resized when needed.
 
-//slices 1:07:18
+//for loop 1:15:27
 //https://www.youtube.com/watch?v=yyUHQIec83I&t=3078s
 
 //infinite loop - keeping tab of people who book tickets as well as the number of tickets sold
